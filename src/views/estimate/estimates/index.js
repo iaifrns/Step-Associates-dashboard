5bd2e4fd0f9a0137b40d8324e8f1e5b4957e5c65
iaifrns <nsinifranc@gmail.com>
2024-05-14 12:04:02 +0100

    changes in the app

---
+++ src/views/estimate/estimates/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+
+import EnhancedTable from './table';
+
+const Estimates = () => {
+  return (
+    <React.Fragment>
+      <Row>
+        <EnhancedTable/>
+      </Row>
+    </React.Fragment>
+  );
+};
+
+export default Estimates;
